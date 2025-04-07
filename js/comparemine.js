29a2e7e23356a8871d9d6dd6c45a1137b6af3450
Emerson Carvalho <emerson.acba@gmail.com>
2025-04-07 17:58:33 -0300

    Atualizando

--- js/comparemine.js
+++ js/comparemine.js
@@ -1,18 +1,16 @@
 function comparemine() {
-    // Obtém todos os checkboxes
-    let checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
-
     let ids_checkboxes = [];
-    
-    // Contador para os checkboxes checados
+    let checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
     let checkedCount = 0;
-    
+
     checkboxes.forEach(function(checkbox) {
         if (checkbox.checked) {
             checkedCount++;  
             ids_checkboxes.push(checkbox.id);  
         }
     });
+
+    console.log(ids_checkboxes);
 
     if (checkedCount === 0) {
         alert("Você não selecionou nenhum veículo para comparação. Por favor, selecione 2 opções.");
@@ -21,43 +19,45 @@ function comparemine() {
     } else if (checkedCount > 2) {
         alert("Você selecionou mais de dois veículos para comparação. Por favor, selecione apenas 2 opções.");    
     } else {
-        let { veiculo1, veiculo2 } = SetCarToCompare(ids_checkboxes); // Captura os veículos
-
-        ShowCompare();
-
-
-        // Preenche os dados do veículo 1
-        document.getElementById("vehicle1-modelo").textContent = veiculo1.modelo;
-        document.getElementById("vehicle1-alturaCacamba").textContent = veiculo1.alturaCacamba;
-        document.getElementById("vehicle1-alturaVeiculo").textContent = veiculo1.alturaVeiculo;
-        document.getElementById("vehicle1-alturaSolo").textContent = veiculo1.alturaSolo;
-        document.getElementById("vehicle1-capacidadeCarga").textContent = veiculo1.capacidadeCarga;
-        document.getElementById("vehicle1-motor").textContent = veiculo1.motor;
-        document.getElementById("vehicle1-potencia").textContent = veiculo1.potencia;
-        document.getElementById("vehicle1-volumeCacamba").textContent = veiculo1.volumeCacamba;
-        document.getElementById("vehicle1-roda").textContent = veiculo1.roda;
-        document.getElementById("vehicle1-preco").textContent = veiculo1.preco;
-        
-        // Preenche os dados do veículo 2
-        document.getElementById("vehicle2-modelo").textContent = veiculo2.modelo;
-        document.getElementById("vehicle2-alturaCacamba").textContent = veiculo2.alturaCacamba;
-        document.getElementById("vehicle2-alturaVeiculo").textContent = veiculo2.alturaVeiculo;
-        document.getElementById("vehicle2-alturaSolo").textContent = veiculo2.alturaSolo;
-        document.getElementById("vehicle2-capacidadeCarga").textContent = veiculo2.capacidadeCarga;
-        document.getElementById("vehicle2-motor").textContent = veiculo2.motor;
-        document.getElementById("vehicle2-potencia").textContent = veiculo2.potencia;
-        document.getElementById("vehicle2-volumeCacamba").textContent = veiculo2.volumeCacamba;
-        document.getElementById("vehicle2-roda").textContent = veiculo2.roda;
-        document.getElementById("vehicle2-preco").textContent = veiculo2.preco.toLocaleString('pt-BR');
+        ShowCompare(ids_checkboxes);
     }
-};
+}
 
 function HideCompare() {
     document.getElementById("popup").style.display = "none";
 }
 
-function ShowCompare() {
+function ShowCompare(ids_checkboxes) {
     document.getElementById("popup").style.display = "block";
+    UpdateCompareTable(ids_checkboxes);
+}
+
+function UpdateCompareTable(ids_checkboxes) {
+    let { veiculo1, veiculo2 } = SetCarToCompare(ids_checkboxes);
+
+    // Preenche os dados do veículo 1
+    document.getElementById("vehicle1-modelo").textContent = veiculo1.modelo;
+    document.getElementById("vehicle1-alturaCacamba").textContent = veiculo1.alturaCacamba;
+    document.getElementById("vehicle1-alturaVeiculo").textContent = veiculo1.alturaVeiculo;
+    document.getElementById("vehicle1-alturaSolo").textContent = veiculo1.alturaSolo;
+    document.getElementById("vehicle1-capacidadeCarga").textContent = veiculo1.capacidadeCarga;
+    document.getElementById("vehicle1-motor").textContent = veiculo1.motor;
+    document.getElementById("vehicle1-potencia").textContent = veiculo1.potencia;
+    document.getElementById("vehicle1-volumeCacamba").textContent = veiculo1.volumeCacamba;
+    document.getElementById("vehicle1-roda").textContent = veiculo1.roda;
+    document.getElementById("vehicle1-preco").textContent = veiculo1.preco;
+
+    // Preenche os dados do veículo 2
+    document.getElementById("vehicle2-modelo").textContent = veiculo2.modelo;
+    document.getElementById("vehicle2-alturaCacamba").textContent = veiculo2.alturaCacamba;
+    document.getElementById("vehicle2-alturaVeiculo").textContent = veiculo2.alturaVeiculo;
+    document.getElementById("vehicle2-alturaSolo").textContent = veiculo2.alturaSolo;
+    document.getElementById("vehicle2-capacidadeCarga").textContent = veiculo2.capacidadeCarga;
+    document.getElementById("vehicle2-motor").textContent = veiculo2.motor;
+    document.getElementById("vehicle2-potencia").textContent = veiculo2.potencia;
+    document.getElementById("vehicle2-volumeCacamba").textContent = veiculo2.volumeCacamba;
+    document.getElementById("vehicle2-roda").textContent = veiculo2.roda;
+    document.getElementById("vehicle2-preco").textContent = veiculo2.preco;
 }
 
 function SetCarToCompare(ids_checkboxes) {
@@ -75,21 +75,20 @@ function SetCarToCompare(ids_checkboxes) {
             this.roda = roda;
             this.preco = preco;
         }
+
+        static info_veiculos = [
+            new Carro("Ford Ranger XL Cabine 2022", 0.51, 1.81, 0.232, 1020, "2.2 Diesel", 160, 1.1, "16\"", "R$ 150.000"),
+            new Carro("Ford Ranger XLS 2.2 Diesel 2022", 0.53, 1.82, 0.237, 1050, "2.2 Diesel Turbo", 200, 1.15, "17\"", "R$ 175.000"),
+            new Carro("Ford Ranger Storm 2022", 0.55, 1.83, 0.242, 1100, "3.2 Diesel Turbo", 250, 1.2, "18\"", "R$ 210.000")
+        ];
+
+        static getCarDataById(id) {
+            return Carro.info_veiculos[parseInt(id)];
+        }
     }
 
-    let info_veiculos = [
-        new Carro("Ford Ranger XL Cabine 2022", 0.51, 1.81, 0.232, 1020, "2.2 Diesel", 160, 1.1, "16\"", "R$ 150.000"),
-        new Carro("Ford Ranger XLS 2.2 Diesel 2022", 0.53, 1.82, 0.237, 1050, "2.2 Diesel Turbo", 200, 1.15, "17\"", "R$ 175.000"),
-        new Carro("Ford Ranger Storm 2022", 0.55, 1.83, 0.242, 1100, "3.2 Diesel Turbo", 250, 1.2, "18\"", "R$ 210.000")
-    ];
+    let veiculo1 = Carro.getCarDataById(parseInt(ids_checkboxes[0]) - 1);
+    let veiculo2 = Carro.getCarDataById(parseInt(ids_checkboxes[1]) - 1);
 
-    function getCarDataById(id) {
-        id = parseInt(id);
-        return info_veiculos[id];
-    }
-
-    let veiculo1 = getCarDataById(parseInt(ids_checkboxes[0]) - 1);
-    let veiculo2 = getCarDataById(parseInt(ids_checkboxes[1]) - 1);
-    
     return { veiculo1, veiculo2 };
 }
--- js/form.js
+++ js/form.js
@@ -1,27 +1,99 @@
+document.getElementById("Input_Name").focus();
 
-//class contato
+function Enviar(event) {
+    event.preventDefault(); // <-- Isso evita que a página recarregue
 
-class contato {
-    
-}
-
-function Post(form) {
-
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
-}
-
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    // Criando a classe contato
+    class Contato {
+        constructor(nome, telefone, email, mensagem) {
+            this.nome = nome;
+            this.telefone = telefone;
+            this.email = email;
+            this.mensagem = mensagem;
+        }
     }
 
+    // Pegando os dados dos inputs
+    const nome = document.getElementById("Input_Name").value.trim();
+    const telefone = document.getElementById("Input_Telefone").value.trim();
+    const email = document.getElementById("Input_Email").value.trim();
+    const mensagem = document.getElementById("mensagem_contact").value.trim();
+
+    // Validações básicas
+    if (!nome) {
+        alert("Por favor, informe o seu nome!");
+        return document.getElementById("Input_Name").focus();
+    }
+
+    if (!telefone) {
+        alert("Por favor, informe o seu telefone!");
+        return document.getElementById("Input_Telefone").focus();
+    }
+
+    if (!email) {
+        alert("Por favor, informe o seu E-mail!");
+        return document.getElementById("Input_Email").focus();
+    }
+
+    if (!email.includes("@") || !email.includes(".") || email.includes(" ")) {
+        alert("Por favor, informe um e-mail válido!");
+        return document.getElementById("Input_Email").focus();
+    }
+
+    if (!mensagem) {
+        alert("Por favor, digite a sua mensagem!");
+        return document.getElementById("mensagem_contact").focus();
+    }
+
+    // Criando objeto com os dados do contato
+    const data = new Contato(nome, telefone, email, mensagem);
+
+    // Mostrando mensagem de sucesso
+    document.getElementById("popup-contact").style.display = "flex";
+    document.getElementById("msg_obrigado").innerHTML = `
+        Obrigado sr(a) ${data.nome}! Os seus dados foram encaminhados com sucesso.<br><br>
+        Responderemos a você no e-mail: ${data.email} ou pelo telefone ${data.telefone}.
+    `;
 }
+
+// Função para resetar o formulário
+function resetForm() {
+    document.getElementById("form-contact").reset();
+}
+
+// Função para fechar o popup e limpar o formulário
+function closePopup() {
+    document.getElementById("popup-contact").style.display = "none";
+    resetForm();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("form-contact");
+    form.addEventListener("submit", Enviar);
+});
+
+ const bttn_send = document.getElementById("bttn_send_message");
+const span1 = document.getElementById("spanCheckbox1");
+const span2 = document.getElementById("spanCheckbox2");
+const checkbox1 = document.getElementById("inputCheckboxAccept");
+const checkbox2 = document.getElementById("inputCheckboxNewsLetter");
+
+
+span1.addEventListener("click",function(){
+    if(checkbox1.checked){
+        checkbox1.checked = false;
+        bttn_send.disabled = true;
+    } else {checkbox1.checked = true; bttn_send.disabled = false}
+});
+
+checkbox1.addEventListener("change",function(){
+    if(checkbox1.checked){
+        bttn_send.disabled = false;
+    } else {bttn_send.disabled = true}
+});
+
+span2.addEventListener("click",function(){
+    if(checkbox2.checked){
+        checkbox2.checked = false;
+    } else {checkbox2.checked = true}
+});
